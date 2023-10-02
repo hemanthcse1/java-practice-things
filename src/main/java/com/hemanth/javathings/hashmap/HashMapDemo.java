e1a7c8ce06cf9098bddf11b1ef57c811427db45c
hemanthcse1 <hemanthcse1@gmail.com>
2023-10-02 21:03:42 +0530

    HashMap practice different functions

--- src/main/java/com/hemanth/javathings/hashmap/HashMapDemo.java
+++ src/main/java/com/hemanth/javathings/hashmap/HashMapDemo.java
@@ -15,5 +15,33 @@ public class HashMapDemo {
 
 
         System.out.println(impIDs);
+
+        // case sensitive key
+        impIDs.put("hemanth", 4);
+        System.out.println(impIDs);
+
+        // override value with existing key
+        impIDs.put("hemanth", 5);
+        System.out.println(impIDs);
+
+        // return if key is present
+        System.out.println(impIDs.containsKey("hemanth"));
+
+        // return if value is present
+        System.out.println(impIDs.containsValue(5));
+
+        // replace
+        impIDs.replace("hemanth", 6);
+        System.out.println(impIDs);
+
+        // replace function with new key
+        impIDs.replace("kumar", 6);
+        System.out.println(impIDs);
+
+        // put if absent
+        impIDs.putIfAbsent("kumar",7);
+        System.out.println(impIDs);
+
+
     }
 }
